8e587d53469f972316ba2b47c9afc950808fdfbc
TeAcHaCk <hackingtechtricks@gmail.com>
2023-04-17 00:59:41 +0530

    fifthCommit

--- app/src/main/java/com/example/inventory_app_20/Item_Add_Fragment.java
+++ app/src/main/java/com/example/inventory_app_20/Item_Add_Fragment.java
@@ -1,24 +1,102 @@
 package com.example.inventory_app_20;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.squareup.picasso.Picasso;
 
 public class Item_Add_Fragment extends Fragment {
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+    private Button ButtonChooseImage;
+    private Button ButtonUpload;
+    private TextView mTextViewShowUploads;
+    private EditText ItemName;
+    private EditText ItemCategory;
+    private ImageView mImageView;
+    private ProgressBar mProgressBar;
+
+    ActivityResultLauncher<String> mtakepic;
+
+    private Uri mImageUri;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     public Item_Add_Fragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==100 && data !=null && data.getData()!=null ){
+           mImageUri = data.getData();
+            Picasso.get().load(mImageUri).into(mImageView);
+        }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,100);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_item__add_, container, false);
+        ButtonChooseImage = view.findViewById(R.id.button_choose_image);
+        ButtonUpload = view.findViewById(R.id.button_upload);
+        mTextViewShowUploads = view.findViewById(R.id.text_view_show_uploads);
+        ItemName = view.findViewById(R.id.item_name);
+        ItemCategory = view.findViewById(R.id.item_category);
+        mImageView = view.findViewById(R.id.image_view);
+        mProgressBar = view.findViewById(R.id.progress_bar);
+
+        ButtonChooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChooser();
+            }
+        });
+        ButtonUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        mTextViewShowUploads.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_item__add_, container, false);
+        return view;
     }
 }
--- app/src/main/java/com/example/inventory_app_20/MainActivity.java
+++ app/src/main/java/com/example/inventory_app_20/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.inventory_app_20;
 
-import androidx.activity.OnBackPressedDispatcher;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +13,6 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
